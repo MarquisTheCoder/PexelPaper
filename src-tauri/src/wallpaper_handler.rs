388305466e7e4c1acc4377ce9dc56c3f1e46d0a9
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-01 20:21:27 -0500

    MarquisTheCoder:  Sun Oct  1 20:21:27 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -13,11 +13,27 @@ impl WallpaperHandler<'_>{
     
     pub fn play(wallpaper: Wallpaper){
         if !wallpaper.get_wallpaper_path().is_none() {
-            println!("wallpaper path exist")
+            match !wallpaper.get_wallpaper_path(){
+                Some(wallpaper_path) =>{
+                    const vlc_executable = "/Applications/VLC.app/Contents/MacOS/VLC";
+                    const video_wallpaper_flag: &str = "--video-wallpaper";
+                    const no_osd: &str = "--no-osd";
+                    const loop_playback: &str = "-L";
+                
+                    let  mut run_wallpaper_in_background = Command::new(vlc_executable)
+                        .arg(video_wallpaper_flag)
+                        .arg(wallpaper_path)
+                        .arg(loop_playback)
+                        .arg(no_osd)
+                        .spawn()
+                        .expect("[-] Cannot run video in the background") 
+                }
+            }
         }
     }
 }
 
 fn main(){
     let wallpaper: Wallpaper = Wallpaper::new("path");
+    WallpaperHandler::play(wallpaper);
 }
