45c1a56419a711c1a47e888893dccae056e72969
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-01 20:39:42 -0500

    MarquisTheCoder:  Sun Oct  1 20:39:42 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -16,13 +16,14 @@ impl WallpaperHandler<'_>{
             match wallpaper.get_wallpaper_path(){
                 Some(wallpaper_path) =>{
                     
-                    let full_path: &str = format!("/Applications/VLC.app/Contents/MacOS/ --video-wallpaper {} --no-osd -L", wallpaper_path);
+                    let full_path: String = format!("/Applications/VLC.app/Contents/MacOS/ --video-wallpaper {} --no-osd -L", wallpaper_path);
 
                     let  mut run_wallpaper_in_background = Command::new(full_path)
                         .spawn()
                         .expect("[-] Cannot run video in the background"); 
                 },
-                None => println!("Wallpaper path does not exist");
+                None => println!("Wallpaper path does not exist"),
+                
             }
         }
     }
