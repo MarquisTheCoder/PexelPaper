1d68f054fec8342262cacb23312477a13e3548d0
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-21 22:59:50 -0500

    MarquisTheCoder:  Sat Oct 21 22:59:50 CDT 2023

--- src-tauri/src/wallpaper.rs
+++ src-tauri/src/wallpaper.rs
@@ -93,7 +93,7 @@ fn main(){
     }
     println!("playing the wallpaper now");
    wallpaper1.play();
-   let five_seconds = time::Duration::from_millis(5000);
+   let five_seconds = time::Duration::from_millis(10000);
    println!("waiting five seconds");
    thread::sleep(five_seconds);
    wallpaper1.kill();
