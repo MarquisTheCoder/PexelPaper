781f4eeaa51188a03f3021becb82be7e3819f50e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-22 03:14:05 -0500

    MarquisTheCoder:  Sun Oct 22 03:14:05 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -18,23 +18,27 @@ impl<'b> WallpaperHandler<'static>{
     }
 
     fn check_current_wallpaper_active(&mut self) -> bool{
-       match self.current_wallpaper.get_wallpaper_pid(){
-        Some => true,
-        None => false
-       }
+        match self.current_wallpaper.get_wallpaper_pid(){
+            Some => true,
+            None => false
+        }
     }
+
     fn kill_current_wallpaper(&mut self){
         self.current_wallpaper.kill();
-    } 
+    }
 
     pub fn set_current_wallpaper(&mut self, wallpaper: Wallpaper){
         
         if(self.check_current_wallpaper_active()){
             self.kill_current_wallpaper();
-        }
+        };
         self.current_wallpaper = &wallpaper;
-    } 
+    }
 
+    pub fn run_current_wallpaper(&mut self){
+        self.current_wallpaper.play();
+    }
 }
 
 fn main(){
