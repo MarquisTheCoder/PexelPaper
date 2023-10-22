fb42f751c05194ab3a61f981804bb2492d4d6180
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-21 22:40:29 -0500

    MarquisTheCoder: working on seperating concerns of wallpaper functions and the wallpaper handler Sat Oct 21 22:40:29 CDT 2023

--- src-tauri/src/wallpaper.rs
+++ src-tauri/src/wallpaper.rs
@@ -32,23 +32,35 @@ impl Wallpaper{
         self.wallpaper_pid = Some(pid);
     }
 
-    pub fn play_wallpaper(mut self){
+    pub fn play_wallpaper(&mut self){
 
         const VLC_EXECUTABLE: &str = "/Applications/VLC.app/Contents/MacOS/VLC";
         const VIDEO_WALLPAPER: &str = "--video-wallpaper";
+        const NO_AUDIO: &str = "--noaudio";
         const NO_OSD: &str = "--no-osd";
         const LOOP_PLAYBACK: &str = "-L";
         let video_path: &str =  "";
 
-        let run_wallpaper_in_background = Command::new(VLC_EXECUTABLE)
-            .arg(VIDEO_WALLPAPER)
-            .arg(video_path)
-            .arg(LOOP_PLAYBACK)
-            .arg(NO_OSD)
-                .spawn()
-                .expect("[-] Cannot run video in the background");
+        match self.get_wallpaper_path(){
+            Some(wallpaper_path) =>{
+                println!("making sure I'm getting the correct path: {}", wallpaper_path); 
+                
+                let run_wallpaper_in_background = Command::new(VLC_EXECUTABLE)
+                    .arg(VIDEO_WALLPAPER)
+                    .arg(wallpaper_path)
+                    .arg(NO_AUDIO)
+                    .arg(LOOP_PLAYBACK)
+                    .arg(NO_OSD)
+                    .spawn()
+                    .expect("[-] Cannot run video in the background");
+                
+                //saving current vlc pid so we can close it and rerun it later
+                self.set_wallpaper_pid(run_wallpaper_in_background.id()); 
+        },
+        None => {
+            println!("cannot display wallpaper");
+        }
 
-        self.set_wallpaper_pid(run_wallpaper_in_background.id());
     }
 
     pub fn kill_wallpapr(&self, pid: u32){
