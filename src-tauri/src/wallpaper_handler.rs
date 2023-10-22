3a3f885ae21a105879036b585b197bab3ab9a980
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-22 03:27:21 -0500

    MarquisTheCoder:  Sun Oct 22 03:27:21 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -10,7 +10,7 @@ pub struct WallpaperHandler<'a >{
 
 impl<'b> WallpaperHandler<'static>{
 
-    pub fn new(wallpaper: &Wallpaper) -> WallpaperHandler<'b>{
+    pub fn new(wallpaper: & mut Wallpaper) -> WallpaperHandler<'b>{
         WallpaperHandler{
             current_wallpaper: wallpaper,
         }
