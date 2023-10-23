ea12111c461df5df74f4d6f74ee4594b973b410f
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-22 20:36:17 -0500

    MarquisTheCoder:  Sun Oct 22 20:36:17 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -16,52 +16,51 @@ impl<'b> WallpaperHandler<'static>{
         }
     }
 
-    fn check_current_wallpaper_active(&mut self) -> bool{
-        match self.current_wallpaper.get_wallpaper_pid(){
-            Some(_empty) => true,
-            None => false
-        }
-    }
+    // fn check_current_wallpaper_active(&mut self) -> bool{
+    //     match self.current_wallpaper.get_wallpaper_pid(){
+    //         Some(_empty) => true,
+    //         None => false
+    //     }
+    // }
 
-    fn kill_current_wallpaper(&mut self){
-        self.current_wallpaper.kill();
-    }
+    // fn kill_current_wallpaper(&mut self){
+    //     self.current_wallpaper.kill();
+    // }
 
-    pub fn play_current_wallpaper(&mut self){
-       let wallpaper: Wallpaper = *self.current_wallpaper.clone();
-    }
+    // pub fn play_current_wallpaper(&mut self){
+    //    let wallpaper: Wallpaper = self.current_wallpaper;
+    // }
 
-    pub fn set_current_wallpaper(&mut self, wallpaper: & mut Wallpaper){
+    // pub fn set_current_wallpaper(&mut self, wallpaper: & mut Wallpaper){
         
-        if self.check_current_wallpaper_active(){
-            self.kill_current_wallpaper();
-        };
-        let mut copy = Wallpaper::new("");
-        match wallpaper.get_wallpaper_path(){
-            Some(wallpaper_path) => {
-                copy.set_wallpaper_path(&wallpaper_path);
-            },
-            None => {
-                println!("do nothing");
-            }
-        }
+    //     if self.check_current_wallpaper_active(){
+    //         self.kill_current_wallpaper();
+    //     };
+    //     let mut copy = Wallpaper::new("");
+    //     match wallpaper.get_wallpaper_path(){
+    //         Some(wallpaper_path) => {
+    //             copy.set_wallpaper_path(&wallpaper_path);
+    //         },
+    //         None => {
+    //             println!("do nothing");
+    //         }
+    //     }
 
-        match wallpaper.get_wallpaper_pid(){
-            Some(wallpaper_pid) => {
-                copy.set_wallpaper_pid(wallpaper_pid);
-            },
-            None => {
-                println!("do nothing");
-            }
-        }
+    //     match wallpaper.get_wallpaper_pid(){
+    //         Some(wallpaper_pid) => {
+    //             copy.set_wallpaper_pid(wallpaper_pid);
+    //         },
+    //         None => {
+    //             println!("do nothing");
+    //         }
+    //     }
         
-        *self.current_wallpaper = copy;
-    }
+    //     *self.current_wallpaper = copy;
+    // }
 
 }
 
 fn main(){
-    let  _wallpaper:&mut Wallpaper = &mut Wallpaper::new("/Users/coder/Movies/testWallpaper.mp4");
+    let  _wallpaper: &mut Wallpaper = &mut Wallpaper::new("/Users/coder/Movies/testWallpaper.mp4");
     let wallpaper_handler: WallpaperHandler = WallpaperHandler::new(_wallpaper);
-    wallpaper_handler.play_current_wallpaper();
 }
