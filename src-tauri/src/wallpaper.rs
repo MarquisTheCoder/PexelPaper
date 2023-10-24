190ee85513ab57a8c4761a3383da4173f75851f3
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-24 10:20:34 -0500

    MarquisTheCoder:  Tue Oct 24 10:20:34 CDT 2023

--- src-tauri/src/wallpaper.rs
+++ src-tauri/src/wallpaper.rs
@@ -16,7 +16,7 @@ impl Wallpaper{
             wallpaper_pid: None
         }
     }
-    pub equals(&self, other: &Wallpaper) -> bool{
+    pub fn equals(&self, other: &Wallpaper) -> bool{
         if(self.get_wallpaper_path().is_some() && other.get_wallpaper_path().is_some()){
             let current_wallpaper_path: String = self.get_wallpaper_path().unwrap();
             let other_wallpaper_path: String = other.get_wallpaper_path().unwrap();
