10926c3098e7d5f886d733a2de985de5a77ac547
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-24 09:58:14 -0500

    MarquisTheCoder:  Tue Oct 24 09:58:14 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -5,18 +5,19 @@ use wallpaper::Wallpaper;
 // I dont need to make this asynchronous I can just close and re run pids I over complicated the process
 
 pub struct WallpaperHandler<'a >{
-    pub current_wallpaper: &'a mut Wallpaper,
+    pub current_wallpaper: mut Option<Wallpaper>,
 }
 
 impl<'b> WallpaperHandler<'_>{
-
-    pub fn new(wallpaper: &'b mut Wallpaper) -> WallpaperHandler<'b>{
+    static EMPTY_WALLPAPER: mut Wallpaper = Wallpaper::new("none");
+    pub fn new(wallpaper: &'b mut Wallpaper ) -> WallpaperHandler<'b>{
         WallpaperHandler{
-            current_wallpaper: wallpaper,
+            current_wallpaper: None,
         }
     }
 
-    pub fn print_wallpaper(&self){
+    //proud of it
+    fn print_wallpaper(&self){
         match self.current_wallpaper.get_wallpaper_path(){
             Some(wallpaper_path) => {
                 println!("{}", wallpaper_path);
@@ -34,40 +35,30 @@ impl<'b> WallpaperHandler<'_>{
         }
     }
 
-    // fn kill_current_wallpaper(&mut self){
-    //     self.current_wallpaper.kill();
-    // }
+    pub fn kill_current_wallpaper(&mut self){
+        self.current_wallpaper.kill();
+    }
 
     pub fn play_current_wallpaper(&mut self){
        self.current_wallpaper.play()
     }
 
-    // pub fn set_current_wallpaper(&mut self, wallpaper: & mut Wallpaper){
+    pub fn set_current_wallpaper(&mut self, wallpaper: & mut Wallpaper){
         
-    //     if self.check_current_wallpaper_active(){
-    //         self.kill_current_wallpaper();
-    //     };
-    //     let mut copy = Wallpaper::new("");
-    //     match wallpaper.get_wallpaper_path(){
-    //         Some(wallpaper_path) => {
-    //             copy.set_wallpaper_path(&wallpaper_path);
-    //         },
-    //         None => {
-    //             println!("do nothing");
-    //         }
-    //     }
+        if self.check_current_wallpaper_active(){
+            self.kill_current_wallpaper();
+        };
+        let mut copy = Wallpaper::new("");
+        match wallpaper.get_wallpaper_path(){
+            Some(wallpaper_path) => {
+                copy.set_wallpaper_path(&wallpaper_path);
+            },
+            None => {
+                println!("do nothing");
+            }
+        }
 
-    //     match wallpaper.get_wallpaper_pid(){
-    //         Some(wallpaper_pid) => {
-    //             copy.set_wallpaper_pid(wallpaper_pid);
-    //         },
-    //         None => {
-    //             println!("do nothing");
-    //         }
-    //     }
-        
-    //     *self.current_wallpaper = copy;
-    // }
+    }
 
 }
 
