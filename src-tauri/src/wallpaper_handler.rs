d077c2dbc5fcca60a3eeb3e45408e8179f6ad1e9
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-24 09:35:55 -0500

    MarquisTheCoder:  Tue Oct 24 09:35:55 CDT 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -74,5 +74,5 @@ impl<'b> WallpaperHandler<'static>{
 fn main(){
     let  _wallpaper:Wallpaper = Wallpaper::new("/Users/coder/Movies/testWallpaper.mp4");
     let wallpaper_handler: WallpaperHandler = WallpaperHandler::new(_wallpaper);
-    wallpaper_handler.print_wallpaper(_wallpaper);
+    wallpaper_handler.print_wallpaper(&mut _wallpaper);
 }
