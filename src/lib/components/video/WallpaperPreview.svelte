380bc36ab0327eb13d5c8fa0ece8e5417217dea6
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-26 08:54:51 -0500

    MarquisTheCoder: adding fake wallpapers Thu Oct 26 08:54:51 CDT 2023

---
+++ src/lib/components/video/WallpaperPreview.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div>
+
+</div>
+
+<style>
+    div{
+        width: 30%;
+        height: 200px;
+        background-color: #2d2c32;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,30 @@
     import "../app.css";
     import Titlebar from "$lib/components/window/Titlebar.svelte"
     import Window from "$lib/components/window/Window.svelte"
+    import WallpaperPreview from "$lib/components/video/WallpaperPreview.svelte"
 </script>
 
 <Titlebar/>
 <Window>
     <section id="path-selection">
-       <input/> 
     </section>
-    <section></section>
+    <section id="wallpaper-previews">
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+        <WallpaperPreview />
+    </section>
 </Window>
 
 <style>
@@ -19,10 +35,21 @@
         font-size: 10rem;
     }
     #path-selection{
-        height: 50px;
+        height: 80px;
         width: 100vw;
         background-color: red;
         display: flex;
         flex-direction: row;
+    }
+    #wallpaper-previews{
+        padding: 30px 10px 30px 10px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 25px;
+        height: 100vh;
+        max-height: 100vh;
+        width: 700px;
+        overflow-y: scroll;
     }
 </style>
