0377f1bb750779debe5be3e97e76b2d179bcebbb
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-27 00:22:50 -0500

    MarquisTheCoder:  Fri Oct 27 00:22:50 CDT 2023

--- src-tauri/src/list_wallpapers.rs
+++ src-tauri/src/list_wallpapers.rs
@@ -5,11 +5,33 @@ use std::path::{Path, PathBuf};
 use std::fs::{self, DirEntry, File, metadata};
 use std::vec;
 
-pub fn listFiles(){
+pub fn list_wallpapers(folder_path_string: &str){
     let vital_file_extensions = [
         ".mov",
         ".mp4",
         ".avif",
     ];
+    let found_wallpapers: &mut Vec<String>;
+
+    folder_path: &Path = Path::new(format!("{}",folder_path_string));
     
+    if metadata(&folder_path)?.is_dir(){
+
+        let paths: ReadDir = fs::read_dir(&folder_path);
+
+        for path_results: Result<DirEntry, Error> in paths{
+
+            let full_path: PathBuf = path_result?.path();
+            let file_name: &OsStr = full_path.file_name().unwrap();
+            let file_name_str: &str = file_name.to_str().unwrap();
+
+            // if !metadata(&full_path)?.is_dir(){
+                
+            // }
+        }
+
+    }
+
+
+
 }
