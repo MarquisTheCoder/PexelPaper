8a319f838a26406cc1880dfad539e81a1648a35e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-14 09:04:20 -0600

    MarquisTheCoder:  Tue Nov 14 09:04:20 CST 2023

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script>
     // import { appWindow } from '@tauri-apps/api/window'
-    import Titlebar from "$lib/components/window/Titlebar.svelte";
     import Window from "$lib/components/window/Window.svelte";
     import "../app.css";
 </script>
 
 <Window>
-    <Titlebar/>
-    <!-- <PathSelection/>
-    <WallpaperArea>
+    <!-- <Titlebar/> -->
+    <!-- <PathSelection/> -->
+    <!-- <WallpaperArea>
         {#each $wallpaper_store as path}
             <svelte:component this={Wallpaper} src={""} {path}/>
         {/each}
