fe52e00b6ec9b1a9d47082cb9b2f37b571227c11
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-12 13:59:05 -0600

    MarquisTheCoder: "" | Tue Dec 12 13:59:05 CST 2023

--- src-tauri/src/wallpaper_handler.rs
+++ src-tauri/src/wallpaper_handler.rs
@@ -24,7 +24,7 @@ impl<'b> WallpaperHandler<'_>{
     
     // static EMPTY_WALLPAPER: Wallpaper = Wallpaper::new("default");
     pub fn new(wallpaper: &'b str) -> WallpaperHandler<'b>{
-        let &mut _wallpaper:Wallpaper = Wallpaper::new(wallpaper);
+        let _wallpaper:Wallpaper = &mut  Wallpaper::new(wallpaper);
         WallpaperHandler{
             current_wallpaper: _wallpaper,
         }
