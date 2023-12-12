2c2a426ad9d1525adab5bf66b16da2ca84eae326
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-12 16:21:03 -0600

    MarquisTheCoder: "" | Tue Dec 12 16:21:03 CST 2023

--- src-tauri/src/wallpaper.rs
+++ src-tauri/src/wallpaper.rs
@@ -49,7 +49,7 @@ impl Wallpaper{
         const NO_AUDIO: &str = "--noaudio";
         const NO_OSD: &str = "--no-osd";
         const LOOP_PLAYBACK: &str = "-L";
-        const NO_GUI: &str = "-I dummy --dummy-quiet";
+        const NO_GUI: &str = "--key-wallpaper";
 
         match self.get_wallpaper_path(){
             Some(wallpaper_path) => {
