3f006cf317dce97b375d7fc15bed93aaf60ffb29
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2024-09-13 19:50:21 -0500

    MarquisTheCoder: "" | Fri Sep 13 19:50:21 CDT 2024

--- src-tauri/src/grab_wallpaper_image.rs
+++ src-tauri/src/grab_wallpaper_image.rs
@@ -4,16 +4,16 @@ use std::string::FromUtf8Error;
 
 
 /*ffmpeg -loglevel quiet -ss 26 -i 3196505-sd_960_540_30fps.mp4 -t 1  -f image2 - */
-pub fn grab_wallpaper_image(wallpaper_video_path: &str){
+pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<String, FromUtf8Error>{
     let raw_image_output = Command::new("ffmpeg")
         .args(["-loglevel", "quiet", "-ss", "26", "-i", wallpaper_video_path, "-t", "1", "-f", "image2", "-"])
         .output()
         .expect("failed to grab image");
 
-    println!( raw_image_output.stdout);
+    return String::from_utf8(raw_image_output.stdout);
     
 }
 
 fn main(){
-    grab_wallpaper_image("/Users/coder/Wallpapers/3196505-sd_960_540_30fps.mp4");
+    let wallpaper = grab_wallpaper_image("/Users/coder/Wallpapers/3196505-sd_960_540_30fps.mp4");
 }
