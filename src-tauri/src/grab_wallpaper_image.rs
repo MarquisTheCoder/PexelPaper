30d3f74cd41bd77a2f0e970b06e9319b7af115b8
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2024-09-14 18:09:33 -0500

    MarquisTheCoder: "" | Sat Sep 14 18:09:33 CDT 2024

--- src-tauri/src/grab_wallpaper_image.rs
+++ src-tauri/src/grab_wallpaper_image.rs
@@ -1,7 +1,8 @@
 
 use std::process::Command;
 use std::string::FromUtf8Error;
-
+use base64::{encode};
+use std::io;
 
 /*ffmpeg -loglevel quiet -ss 26 -i 3196505-sd_960_540_30fps.mp4 -t 1  -f image2 - */
 pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<String, FromUtf8Error>{
@@ -10,7 +11,11 @@ pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<String, FromUt
         .output()
         .expect("failed to grab image");
 
-    return String::from_utf8(raw_image_output.stdout);
+     // Encode the binary image data as base64
+     let base64_image = encode(raw_image_output.stdout);
+    
+     // Return the base64-encoded string
+     Ok(base64_image)
     
 }
 
