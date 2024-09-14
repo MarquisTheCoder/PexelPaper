ce614e651178b51e64e3f5975f833a85be8b4794
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2024-09-13 19:45:20 -0500

    MarquisTheCoder: "" | Fri Sep 13 19:45:20 CDT 2024

--- src-tauri/src/grab_wallpaper_image.rs
+++ src-tauri/src/grab_wallpaper_image.rs
@@ -4,7 +4,7 @@ use std::string::FromUtf8Error;
 
 
 /*ffmpeg -loglevel quiet -ss 26 -i 3196505-sd_960_540_30fps.mp4 -t 1  -f image2 - */
-pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<&str, FromUtf8Error>{
+pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<String, FromUtf8Error>{
     let raw_image_output = Command::new("ffmpeg")
         .args(["-loglevel", "quiet", "-ss", "26", "-i", wallpaper_video_path, "-t", "1", "-f", "image2", "-"])
         .output()
