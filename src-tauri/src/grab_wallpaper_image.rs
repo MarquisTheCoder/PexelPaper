491a69238ed0772f1e85d9b153504d74dd9d61a9
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2024-09-14 20:03:08 -0400

    MarquisTheCoder: "" | Sat Sep 14 20:03:08 EDT 2024

--- src-tauri/src/grab_wallpaper_image.rs
+++ src-tauri/src/grab_wallpaper_image.rs
@@ -8,26 +8,20 @@ use base64::{engine::general_purpose, Engine as _};
 use std::io;
 
 /*ffmpeg -loglevel quiet -ss 26 -i 3196505-sd_960_540_30fps.mp4 -t 1  -f image2 - */
-pub fn grab_wallpaper_image(wallpaper_video_path: &str) -> Result<String, FromUtf8Error>{
+pub fn grab_wallpaper_image(wallpaper_video_path: &str){
     let raw_image_output = Command::new("ffmpeg")
         .args(["-loglevel", "quiet", "-ss", "26", "-i", wallpaper_video_path, "-t", "1", "-f", "image2", "-"])
         .output()
         .expect("failed to grab image");
 
      // Encode the binary image data as base64
-     let base64_image = general_purpose::STANDARD.encode(raw_image_output.stdout);
-    
-     // Return the base64-encoded string
-     Ok(base64_image)
+     let base64_image = general_purpose::STANDARD.encode(&raw_image_output.stdout);
+     println!("{}", base64_image);
     
 }
 
 fn main(){
-    let wallpaper_result = grab_wallpaper_image("/Users/coder/Wallpapers/3196505-sd_960_540_30fps.mp4");
+    let _wallpaper_result = grab_wallpaper_image("/Users/coder/Wallpapers/3196505-sd_960_540_30fps.mp4");
 
-    match wallpaper_result {
-        Ok(wallpaper) => println!("Wallpaper data: {}", wallpaper),
-        Err(e) => println!("Error: {}", e),
-    }
     
 }
